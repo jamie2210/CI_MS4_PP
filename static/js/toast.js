816a6786e646e69232bff08498f74afd27b16c3e
jamie2210 <jamieletts2210@gmail.com>
2023-06-16 08:40:07 +0000

    Add messages to views and update styling

--- static/js/toast.js
+++ static/js/toast.js
@@ -1,1 +1,17 @@
-$('.toast').toast('show');
+/**
+ * [Toast functionality
+ * Credit: Code Institute project Boutique Ado and Bootstrap Toasts demo ]
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toastElements = document.querySelectorAll('.toast');
+    toastElements.forEach(function (toastElement) {
+        let option = {
+            animation: true,
+            autohide: true,
+            delay: 5000,
+        };
+      var toast = new bootstrap.Toast(toastElement, option);
+      toast.show();
+    });
+  });
