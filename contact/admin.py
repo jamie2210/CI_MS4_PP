20e16ce4f030a917f85ce983d48718bc198dca4c
jamie2210 <jamieletts2210@gmail.com>
2023-06-22 07:23:49 +0000

    Add contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+
+class ContactFormAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'contact_name',
+        'contact_email',
+        'contact_phone_number',
+        'contact_subject',
+        'contact_message',
+        'date',
+    )
+
+    fields = (
+        'contact_name',
+        'contact_email',
+        'contact_phone_number',
+        'contact_subject',
+        'contact_message',
+    )
+
+    list_display = (
+        'contact_phone_number',
+        'contact_subject',
+        'date',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(ContactForm, ContactFormAdmin)
