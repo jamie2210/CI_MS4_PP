6c4120c5f47335766e20632921a1c4e4d98c901b
jamie2210 <jamieletts2210@gmail.com>
2023-06-22 07:00:27 +0000

    created contact form models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    contact_name = models.CharField(
+        max_length=75,
+        null=False,
+        blank=False,
+    )
+    contact_email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False,
+    )
+    contact_phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+    contact_subject = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+    contact_message = models.CharField(
+        max_length=1500,
+        null=False,
+        blank=False,
+    )
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.contact_email
