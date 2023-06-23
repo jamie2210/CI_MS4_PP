6b96e99db148f725c442421eccca6f91ab230542
jamie2210 <jamieletts2210@gmail.com>
2023-06-23 16:24:08 +0000

    add content and javascript to footer, adjust navbar

--- static/js/toast.js
+++ static/js/toast.js
@@ -4,6 +4,7 @@
  */
 
 document.addEventListener('DOMContentLoaded', function () {
+    // toasts functionality
     var toastElements = document.querySelectorAll('.toast');
     toastElements.forEach(function (toastElement) {
         let option = {
@@ -14,4 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
       var toast = new bootstrap.Toast(toastElement, option);
       toast.show();
     });
-  });
+
+  // Updates the copyright year in footer with the current year
+  const year = document.querySelector('#current-year');
+  year.innerHTML = new Date().getFullYear();
+
+});
+
