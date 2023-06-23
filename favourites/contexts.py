cfd23fe04f89655a1bd7d108e21dbe7544db1536
jamie2210 <jamieletts2210@gmail.com>
2023-06-23 17:56:10 +0000

    Continue to build favourites page

---
+++ favourites/contexts.py
@@ -0,0 +1,25 @@
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from .models import Favourites
+
+
+def favourite_contents(request):
+    """
+    A context to display favourites count
+    """
+    try:
+        favourites = get_object_or_404(Favourites, username=request.user.id)
+        favourites_items = favourites.products.all()
+        favourites_count = len(favourites_items)
+    except Http404:
+        favourites_count = None
+
+    context = {
+        'favourites_count': favourites_count,
+    }
+
+    return context
+    context = {}
+
+    return context
--- favourites/views.py
+++ favourites/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    HttpResponse,
+    get_object_or_404
+    )
+from django.contrib import messages
+from products.models import Product
 from django.contrib.auth.decorators import login_required
+from .models import Favourites
 
 
 @login_required
 def view_favourites(request):
     """ A view to return the favourites page """
+    favourites_items_count = 0
+    favourites_items = None
+
     return render(request, 'favourites/favourites.html')
