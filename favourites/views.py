48f5554017c635da8f2b4acb857652603b249d4d
jamie2210 <jamieletts2210@gmail.com>
2023-06-24 06:39:12 +0000

    Add view_favourites logic

--- favourites/views.py
+++ favourites/views.py
@@ -14,7 +14,22 @@ from .models import Favourites
 @login_required
 def view_favourites(request):
     """ A view to return the favourites page """
+
     favourites_items_count = 0
     favourites_items = None
+
+    favourites = Favourites.objects.filter(username=request.user).first()
+    if favourites:
+        favourites_items = favourites.products.all()
+        favourites_items_count = favourites.products.all().count()
+
+    if not favourites_items:
+        messages.info(request, 'Your favourites list is empty!')
+
+    template = 'favourites/favourites.html'
+    context = {
+        'favourites_items': favourites_items,
+        'favourites_items_count': favourites_items_count,
+    }
 
     return render(request, 'favourites/favourites.html')
