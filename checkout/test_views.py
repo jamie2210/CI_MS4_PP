cda16173d9cca754616cdbb08b56adf4b74348bf
jamie2210 <jamieletts2210@gmail.com>
2023-06-29 16:17:25 +0000

    Add Checkout user test

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-from .forms import OrderForm
+from django.conf import settings
+from django.contrib.auth.models import User
+from checkout.models import Order
+from profiles.models import UserProfile
 
 
 class TestCheckoutViews(TestCase):
+    """
+    A class for testing checkout views
+    """
 
     def test_get_checkout_with_empty_bag(self):
         """
@@ -10,3 +16,24 @@ class TestCheckoutViews(TestCase):
         """
         response = self.client.get('/checkout')
         self.assertTrue(response, '/products')
+
+    def setUp(self):
+        """
+        Create test user and a test order
+        """
+        test_user = User.objects.create_user(
+            username='test_user', password='test_password')
+
+        test_user.save()
+        test_user = UserProfile.objects.get(user=test_user)
+
+        Order.objects.create(
+            full_name='Test User',
+            email='test_email@gmail.com',
+            phone_number='123456789',
+            country='IE',
+            town_or_city='Test City',
+            street_address1='Test Address 1',
+            street_address2='Test Address 2',
+            user_profile=test_user
+        )
