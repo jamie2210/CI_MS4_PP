5741ae3217620cf4f0890910f9f236cbf8f56f83
jamie2210 <jamieletts2210@gmail.com>
2023-06-30 10:27:20 +0000

    Adjust auto email and txt

--- contact/views.py
+++ contact/views.py
@@ -55,20 +55,21 @@ def contact(request):
 
 def send_auto_contact_received_email(contact_form):
     """Send the user a confirmation email"""
-    contact_form = ContactForm()
-    cust_email = contact_form.contact_email
-    subject = render_to_string(
-        'contact_confirmation_emails/contact_confirmation_subject.txt',
-        {'form': contact_form})
-    body = render_to_string(
-        'contact_confirmation_emails/contact_confirmation_body.txt',
-        {'form': contact_form, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-    send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email, settings.DEFAULT_FROM_EMAIL]
-        )
+    if contact_form.is_valid():
+        cust_email = contact_form.cleaned_data['contact_email']
+        subject = render_to_string(
+            'contact_confirmation_emails/contact_confirmation_subject.txt',
+            {'form': contact_form})
+        body = render_to_string(
+            'contact_confirmation_emails/contact_confirmation_body.txt',
+            {'form': contact_form,
+             'contact_email': settings.DEFAULT_FROM_EMAIL})
+        send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [cust_email]
+            )
 
 
 def contact_success(request):
