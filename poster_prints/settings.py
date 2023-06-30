e9befb68f41de0ce604cde7691146ceb39b0f902
jamie2210 <jamieletts2210@gmail.com>
2023-06-30 14:15:13 +0000

    Create new SQL database

--- poster_prints/settings.py
+++ poster_prints/settings.py
@@ -20,7 +20,7 @@ if os.path.exists("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production.
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
