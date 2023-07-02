bc8e83a8633ff965c544a3d9916ff3857243204c
jamie2210 <jamieletts2210@gmail.com>
2023-07-02 13:28:17 +0000

    Add spacing toast comments

--- bag/views.py
+++ bag/views.py
@@ -119,7 +119,7 @@ def adjust_bag(request, item_id):
             messages.success(
                     request,
                     f'Updated size {size.upper()} {product.name}'
-                    f'quantity to {bag[item_id]["items_by_size"][size]}')
+                    f' quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
@@ -127,7 +127,7 @@ def adjust_bag(request, item_id):
             messages.success(
                     request,
                     f'Removed size {size.upper()} {product.name}'
-                    'from your bag')
+                    ' from your bag')
     if not size:
         if quantity > 0:
             if quantity > product.stock:
