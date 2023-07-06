bd0a04d26f244517520b37db5f535d65e4c29da2
jamie2210 <jamieletts2210@gmail.com>
2023-07-06 15:23:02 +0000

    Fix: Remove irrelevant comment

--- products/views.py
+++ products/views.py
@@ -15,8 +15,6 @@ from .models import Product, Category
 from .forms import ProductForm
 from favourites.models import Favourites
 
-# Create your views here.
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
