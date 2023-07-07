95ed12d83777208db118873da4b233fd5980da56
jamie2210 <jamieletts2210@gmail.com>
2023-07-07 17:13:04 +0000

    Feat: Add admin class for Favourties model

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,27 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 
-# Register your models here.
+# Internal:
+from .models import Favourites
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class FavouritesAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Favourites model.
+    """
+    list_display = (
+        'username',
+    )
+    search_fields = (
+        'username',
+    )
+    list_filter = (
+        'username',
+    )
+    list_per_page = 20
+
+
+admin.site.register(Favourites, FavouritesAdmin)
