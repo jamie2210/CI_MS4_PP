c6d51e1640c306ab4b1abe6975167e23f9585951
jamie2210 <jamieletts2210@gmail.com>
2023-07-08 08:29:20 +0000

    Change DEBUG setting

--- poster_prints/settings.py
+++ poster_prints/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-jamie2210-cims4pp-5sm1mntkjio.ws-eu101.gitpod.io',
